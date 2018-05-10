ab208f57ea5456eeeb6ec6d1c5b9a5fb8fc523d3
Franco Eduardo Morales <franco.e.morales@gmail.com>
2018-05-09 23:42:27 -0300

    Upload project

---
+++ Clima/Clima/Clima/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions;
+using Plugin.MediaManager.Abstractions.Enums;
+using Plugin.MediaManager.Abstractions.EventArguments;
+using Plugin.MediaManager.Abstractions.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Clima
+{
+    public partial class MainPage : ContentPage
+    {
+        private IPlaybackController PlaybackController => CrossMediaManager.Current.PlaybackController;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CrossMediaManager.Current.PlayingChanged += (sender, e) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    ProgressBar.Progress = e.Progress;
+                    Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
+                });
+            };
+        }
+
+
+
+        protected override void OnAppearing()
+        {
+            videoView.Source = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+        }
+
+        void PlayClicked(object sender, System.EventArgs e)
+        {
+            PlaybackController.Play();
+        }
+
+        void PauseClicked(object sender, System.EventArgs e)
+        {
+            PlaybackController.Pause();
+        }
+
+        void StopClicked(object sender, System.EventArgs e)
+        {
+            PlaybackController.Stop();
+        }
+    }
+}
---
+++ Clima/Clima/Clima/TestAudio.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Clima
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestAudio : ContentPage
+    {
+        private IAudioPlayer PlaybackController => CrossMediaManager.Current.AudioPlayer;
+        public TestAudio()
+        {
+            InitializeComponent();
+            CrossMediaManager.Current.PlayingChanged += (sender, e) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    ProgressBar.Progress = e.Progress;
+                    Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
+                });
+            };
+        }
+
+        protected override void OnAppearing()
+        {
+            //videoView.Source = "http://sisteplay1.streamwowza.com:1935/radiohd/RAD25679/playlist.m3u8";
+            //videoView.Source = "https://audioboom.com/posts/5766044-follow-up-305.mp3";
+            //videoView.Source = "http://sc02.yertel.com:8094/;stream.mp3";
+            //videoView.Source = "http://69.175.20.242:8112/live";
+            videoView.Source = "rtsp://sisteplay1.streamwowza.com:1935/radiohd/RAD25679/";
+        }
+
+        void PlayClicked(object sender, System.EventArgs e)
+        {
+            PlaybackController.Play();
+        }
+
+        void PauseClicked(object sender, System.EventArgs e)
+        {
+            PlaybackController.Pause();
+        }
+
+        void StopClicked(object sender, System.EventArgs e)
+        {
+            PlaybackController.Stop();
+        }
+    }
+}
